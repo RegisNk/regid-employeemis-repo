2cb02a8ae4ed798f666a60bad94340308191234b
Regisnk <regisnkundimana77@gmail.com>
2025-01-23 11:38:54 +0200

    798765

--- new_staff/index.php
+++ new_staff/index.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>New Staff MIS</title>
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>WELCOME TO OUR MIS</h1>
-    <a href="registration.php">Go to mis registration</a>
+<body class="bg-light">
+    <div class="container text-center mt-5">
+        <h1 class="mb-4">Welcome to New Staff MIS</h1>
+        <a href="registration.php" class="btn btn-primary">Go to Registration Page</a>
+    </div>
+
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
